ed104416449e52efeaaeaeeb41d76396a94d0328
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-09 21:43:14 -0800

    completed comment.js

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const helpers = require('./utils/helpers');
 const sequelize = require ('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
 const app = express();
 const PORT = process.env.PORT || 3001; 
+
+const hbs = exphbs.create({});
+
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -22,9 +26,12 @@ const sess = {
   
   app.use(session(sess));
 
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(routes);
   
   sequelize.sync({ force: false }).then(() => {
