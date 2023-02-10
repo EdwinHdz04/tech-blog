b306137c0fe7fdcdbaefad02479111b6dcdc2464
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-09 21:51:27 -0800

    fixed index route erros

--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,5 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
-router.use('/api', homeRoutes);
+router.use('/', homeRoutes);
 module.exports = router;
