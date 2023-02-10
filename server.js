80fabea1c6f7faefbf242037c8b361a1419e7bb2
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-09 23:51:39 -0800

    renamed folder layouts

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs =  require('express-handlebars');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
@@ -12,17 +12,22 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001; 
 
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({ helpers: helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
+  secret: 'Super secret secret',
+  cookie: {
+      maxAge: 3000000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
       db: sequelize
-    })
-  };
+  })
+};
   
   app.use(session(sess));
 
